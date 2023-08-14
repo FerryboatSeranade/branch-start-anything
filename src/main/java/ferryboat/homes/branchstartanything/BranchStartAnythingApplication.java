6b1e53b8046a00e1ef58491c6b83b488238a68ea
FerryboatSeranade <ferryboatseranade@gmail.com>
2023-08-14 10:02:18 +0800

    init project

---
+++ src/main/java/ferryboat/homes/branchstartanything/BranchStartAnythingApplication.java
@@ -0,0 +1,13 @@
+package ferryboat.homes.branchstartanything;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchStartAnythingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BranchStartAnythingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ferryboat/homes/branchstartanything/BranchStartAnythingApplicationTests.java
@@ -0,0 +1,13 @@
+package ferryboat.homes.branchstartanything;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranchStartAnythingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
